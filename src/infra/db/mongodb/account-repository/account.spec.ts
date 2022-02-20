d4c89860c2d624452d67272ebee4547168181689
Nypeeh <lammerlammer@outlook.com>
2022-02-20 17:27:38 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -29,6 +29,7 @@ describe('Account Mongo Repository', () => {
     })
 
     expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email')
     expect(account.password).toBe('any_password')
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -13,5 +13,11 @@ export const MongoHelper = {
   },
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+  map (collection: any): any {
+    const { _id, ...collectionWithoudId } = collection
+
+    return Object.assign({}, collectionWithoudId, { id: _id })
   }
+
 }
